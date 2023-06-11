2d19a1909ae47a735e1bf88537bb5b15666f711e
Yuri Kuznecov <yuri.kuznecov@gmail.com>
2023-06-11 00:18:41 +0000

    markers for fix in MetricsProvider

--- tests/UServer/MetricsRaii/sampleMetricsRAII.cpp
+++ tests/UServer/MetricsRaii/sampleMetricsRAII.cpp
@@ -1,33 +1,36 @@
+#include <unistd.h>
+#include <map>
+
 #include "UServerUtils/MetricsHTTPProvider.hpp"
 #include "Generics/CompositeMetricsProvider.hpp"
 #include "Generics/MetricsProvider.hpp"
 #include "UServerUtils/metrics_raii.hpp"
-#include <unistd.h>
+
 int main(int /*argc*/, char** /*argv*/)
 {
+  ReferenceCounting::SmartPtr<Generics::CompositeMetricsProvider> cmp(new Generics::CompositeMetricsProvider);
+  ReferenceCounting::SmartPtr<UServerUtils::MetricsHTTPProvider> m(
+    new UServerUtils::MetricsHTTPProvider(cmp,8081,"/metrics"));
 
-    CompositeMetricsProvider *cmp=new CompositeMetricsProvider;
+  m->activate_object();
+  for(long i=0; i<1000; i++)
+  {
+      {
+          std::map<std::string,std::string> m;
+          m["user_bind_mapper"]="get_user_id";
+          metrics_raii __r(cmp,"user_bind_resolve_calls", m);
+          usleep(rand()%1000 * 1000); /// request imitation
+      }
+      {
+          std::map<std::string,std::string> m;
+          m["user_bind_mapper"]="get_user_id";
+          metrics_raii __r(cmp,"user_bind_add_calls", m);
+          usleep(rand()%1000 * 1000); /// request imitation
+      }
 
-    UServerUtils::MetricsHTTPProvider *m=new UServerUtils::MetricsHTTPProvider(cmp,8081,"/metrics");
-    m->activate_object();
-    for(long i=0; i<1000; i++)
-    {
-        {
-            std::map<std::string,std::string> m;
-            m["user_bind_mapper"]="get_user_id";
-            metrics_raii __r(cmp,"user_bind_resolve_calls", m);
-            usleep(rand()%1000 * 1000); /// request imitation
-        }
-        {
-            std::map<std::string,std::string> m;
-            m["user_bind_mapper"]="get_user_id";
-            metrics_raii __r(cmp,"user_bind_add_calls", m);
-            usleep(rand()%1000 * 1000); /// request imitation
-        }
+  }
+  m->deactivate_object();
+  m->wait_object();
 
-    }
-    m->deactivate_object();
-    m->wait_object();
-    delete m;
-    return 0;
+  return 0;
 }
