d47c360656c23425c57b6efe1953ebb8967f0269
Yuri Kuznecov <yuri.kuznecov@gmail.com>
2023-07-02 22:07:47 +0000

    GeoIP cmake find normalization

--- cmake/Modules/FindGeoIP.cmake
+++ cmake/Modules/FindGeoIP.cmake
@@ -2,52 +2,52 @@
 # - Find GeoIP
 # Find the native GeoIP includes and library
 #
-#  GEOIP_INCLUDE_DIRS - where to find GeoIP.h, etc.
-#  GEOIP_LIBRARIES    - List of libraries when using GeoIP.
-#  GEOIP_FOUND        - True if GeoIP found.
+#  GeoIP_INCLUDE_DIRS - where to find GeoIP.h, etc.
+#  GeoIP_LIBRARIES    - List of libraries when using GeoIP.
+#  GeoIP_FOUND        - True if GeoIP found.
 
-if(GEOIP_INCLUDE_DIRS)
+if(GeoIP_INCLUDE_DIRS)
   # Already in cache, be silent
-  set(GEOIP_FIND_QUIETLY TRUE)
+  set(GeoIP_FIND_QUIETLY TRUE)
 endif()
 
-# Users may set the (environment) variable GEOIP_ROOT
+# Users may set the (environment) variable GeoIP_ROOT
 # to point cmake to the *root* of a directory with include
 # and lib subdirectories for GeoIP
-if(GEOIP_ROOT)
-  set(GEOIP_ROOT PATHS ${GEOIP_ROOT} NO_DEFAULT_PATH)
+if(GeoIP_ROOT)
+  set(GeoIP_ROOT PATHS ${GeoIP_ROOT} NO_DEFAULT_PATH)
 else()
-  set(GEOIP_ROOT $ENV{GEOIP_ROOT})
+  set(GeoIP_ROOT $ENV{GeoIP_ROOT})
 endif()
 
 find_package(PkgConfig)
-pkg_search_module(GEOIP geoip)
+pkg_search_module(GeoIP geoip)
 
 # Find the header
-find_path(GEOIP_INCLUDE_DIR GeoIP.h
+find_path(GeoIP_INCLUDE_DIR GeoIP.h
   HINTS
-    "${GEOIP_INCLUDEDIR}"
-    "${GEOIP_ROOT}"
+    "${GeoIP_INCLUDEDIR}"
+    "${GeoIP_ROOT}"
   PATH_SUFFIXES include Include
 )
 
 # Find the library
-find_library(GEOIP_LIBRARY
+find_library(GeoIP_LIBRARY
   NAMES GeoIP libGeoIP-1
   HINTS
-    "${GEOIP_LIBDIR}"
-    "${GEOIP_ROOT}"
+    "${GeoIP_LIBDIR}"
+    "${GeoIP_ROOT}"
   PATH_SUFFIXES lib
 )
 
-# handle the QUIETLY and REQUIRED arguments and set GEOIP_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set GeoIP_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GEOIP DEFAULT_MSG GEOIP_LIBRARY GEOIP_INCLUDE_DIR)
+find_package_handle_standard_args(GeoIP DEFAULT_MSG GeoIP_LIBRARY GeoIP_INCLUDE_DIR)
 
-if(GEOIP_FOUND)
-  set(GEOIP_LIBRARIES ${GEOIP_LIBRARY})
-  set(GEOIP_INCLUDE_DIRS ${GEOIP_INCLUDE_DIR})
+if(GeoIP_FOUND)
+  set(GeoIP_LIBRARIES ${GeoIP_LIBRARY})
+  set(GeoIP_INCLUDE_DIRS ${GeoIP_INCLUDE_DIR})
 endif()
 
-mark_as_advanced(GEOIP_LIBRARIES GEOIP_INCLUDE_DIRS)
+mark_as_advanced(GeoIP_LIBRARIES GeoIP_INCLUDE_DIRS)
