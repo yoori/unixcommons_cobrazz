e054d7dd1df215635c4a4b72f4b58a27dff5a4f2
Yuri Kuznecov <yuri.kuznecov@gmail.com>
2025-04-14 14:00:12 +0000

    Use installed on host userver by default

--- cmake/SetupUserver.cmake
+++ cmake/SetupUserver.cmake
@@ -1,9 +1,7 @@
-if (${USE_USERVER_PACKAGE})
+if (USE_USERVER_PACKAGE)
   if (NOT userver_FOUND)
-    if (TARGET c-ares)
-      add_library(c-ares::cares ALIAS c-ares)
-    endif()
     find_package(userver REQUIRED)
+    # Add include directory for including internal userver headers
     target_include_directories(userver::core
       INTERFACE /usr/include/userver/
     )
